e2374013c6de2c75258810db32d001c856783a9a
Johannes <johannes.magadani@gmail.com>
2019-03-29 10:30:15 +0200

    Commented out

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -15,21 +15,21 @@ import { FormGroup, FormControl, FormArray } from '@angular/forms';
   ]
 })
 export class NewCourseFormComponent implements OnInit {
-  form = new FormGroup({
-    topics : new FormArray([])
-  });
-  addTopic(topic: HTMLInputElement){
-    (this.form.get('topics') as FormArray).push(new FormControl(topic.value));
-    topic.value = '';
-  }
-  // removeTopic(topic: FormControl){
-  //   let index = this.topics.controls.indexOf(topic);
-  //   this.topics.removeAt(index);
+  // form = new FormGroup({
+  //   topics : new FormArray([])
+  // });
+  // addTopic(topic: HTMLInputElement){
+  //   (this.form.get('topics') as FormArray).push(new FormControl(topic.value));
+  //   topic.value = '';
   // }
+  // // removeTopic(topic: FormControl){
+  // //   let index = this.topics.controls.indexOf(topic);
+  // //   this.topics.removeAt(index);
+  // // }
 
-  get topics(){
-    return this.form.get('topics') as FormArray;
-  }
+  // get topics(){
+  //   return this.form.get('topics') as FormArray;
+  // }
 
   constructor() { }
 
